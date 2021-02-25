0f296d5abb8738644ea649a7b0f3146a72586681
Nathan Lilienthal <nathan@nixpulvis.com>
2021-02-24 20:54:17 -0500

    Start syncing systems.

--- src/bin/sync/eddn.rs
+++ src/bin/sync/eddn.rs
@@ -1,7 +1,8 @@
+use async_std::task;
 use structopt::StructOpt;
 use elite_journal::Event;
 use eddn::{URL, subscribe, Message};
-use galos_db::Database;
+use galos_db::{Database, systems::System};
 use crate::SyncDb;
 
 #[derive(StructOpt, Debug)]
@@ -16,31 +17,45 @@ pub struct Cli {
 impl SyncDb for Cli {
     fn sync_db(&self, db: &Database) {
         for result in subscribe(&self.url) {
-            match result {
-                Ok(envelop) => process_message(envelop.message),
-                // TODO: error! log
-                Err(err) => println!("{}", err),
+            if let Ok(envelop) = result {
+                process_message(db, envelop.message);
+            } else if let Err(err) = result {
+                println!("{}", err);
             }
-        }
+        };
     }
 }
 
-fn process_message(message: Message) {
+fn process_message(db: &Database, message: Message) {
+    task::block_on(async {
     match message {
         Message::Journal(entry) => {
-            match entry.event {
+            if let Some(system) = match entry.event {
                 Event::Location(e) => {
-                    dbg!(e);
+                    Some(e.system)
                 },
                 Event::FsdJump(e) => {
-                    dbg!(e);
+                    Some(e.system)
                 },
-                Event::Docked(e) => {
-                    dbg!(e);
-                },
-                _ => {},
+                _ => None,
+            } {
+                let result = System::create(
+                    db,
+                    system.system_address as i64,
+                    &system.star_system.to_uppercase(),
+                    system.population as i64,
+                    Some(system.system_government),
+                    Some(system.system_allegiance),
+                    Some(system.system_economy),
+                    Some(system.system_second_economy),
+                ).await;
+                match result {
+                    Ok(system) => println!("[EDDN] {}", system.name),
+                    Err(err) => println!("[EDDN] {}", err),
+                }
             }
         },
         _ => {}
     }
+    })
 }
