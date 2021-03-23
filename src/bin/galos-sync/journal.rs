94d2b51bb47707276eed756f3d5a778b0bf58ade
Nathan Lilienthal <nathan@nixpulvis.com>
2021-03-23 09:58:48 -0400

    Allow file and dir paths

--- src/bin/galos-sync/journal.rs
+++ src/bin/galos-sync/journal.rs
@@ -1,7 +1,8 @@
+use std::fs;
 use async_std::task;
 use structopt::StructOpt;
 use indicatif::{ProgressBar, ProgressStyle};
-use elite_journal::entry::{parse_journal_dir, Entry, Event};
+use elite_journal::entry::{self, Event};
 use galos_db::{Database, systems::System};
 use crate::Run;
 
@@ -13,8 +14,16 @@ pub struct Cli {
 
 impl Run for Cli {
     fn run(&self, db: &Database) {
-        // TODO: Support files and dirs.
-        let entries = parse_journal_dir(&self.path).unwrap();
+        let entries = if let Ok(m) = fs::metadata(&self.path) {
+            if m.is_dir() {
+                entry::parse_journal_dir(&self.path).unwrap()
+            } else {
+                entry::parse_journal_file(&self.path).unwrap()
+            }
+        } else {
+            panic!("bad path: {}", self.path);
+        };
+
         let bar = ProgressBar::new(entries.len() as u64);
         bar.set_style(ProgressStyle::default_bar()
             .template("[{elapsed_precise}/{eta_precise}] {bar:40} {pos:>7}/{len:7} ({percent}%) {msg}")
