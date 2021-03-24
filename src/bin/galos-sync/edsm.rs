fbdf8a816fe0fd80e27dbc028105bd77b752b1bc
Nathan Lilienthal <nathan@nixpulvis.com>
2021-03-23 22:03:08 -0400

    Update for moved elite_journal path

--- src/bin/galos-sync/edsm.rs
+++ src/bin/galos-sync/edsm.rs
@@ -2,7 +2,7 @@ use async_std::task;
 use structopt::StructOpt;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use chrono::offset::Utc;
-use elite_journal::Coordinate;
+use elite_journal::system::Coordinate;
 use galos_db::{Database, systems::System};
 use crate::Run;
 
