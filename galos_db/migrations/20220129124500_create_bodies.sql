ecb35b6ff5b70e26c3149947a3026b7e2daf7bbd
Nathan Lilienthal <nathan@nixpulvis.com>
2022-01-31 11:45:11 -0500

    sql lint

--- galos_db/migrations/20220129124500_create_bodies.sql
+++ galos_db/migrations/20220129124500_create_bodies.sql
@@ -1,8 +1,8 @@
 CREATE TABLE bodies (
     address         bigserial  PRIMARY KEY,
     system_address  bigint     REFERENCES systems  NOT NULL,
-    id              smallint   NOT NULL,
     name            varchar    NOT NULL,
+    id              smallint   NOT NULL,
     parent_id       smallint,
     updated_at      timestamp  NOT NULL,
     UNIQUE (system_address, id),
