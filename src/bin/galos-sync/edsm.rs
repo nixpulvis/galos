eee2145efe4ce011375ee75475ed3af94083fb6b
Nathan Lilienthal <nathan@nixpulvis.com>
2024-08-09 12:10:08 -0400

    EDSM file sync

--- src/bin/galos-sync/edsm.rs
+++ src/bin/galos-sync/edsm.rs
@@ -1,7 +1,11 @@
 #![cfg(unix)]
-use crate::Run;
-use galos_db::Database;
 use structopt::StructOpt;
+use indicatif::{ProgressBar, ProgressStyle};
+use async_std::task;
+use chrono::offset::Utc;
+use galos_db::Database;
+use galos_db::systems::System;
+use crate::Run;
 
 #[derive(StructOpt, Debug)]
 pub enum Cli {
@@ -22,7 +26,38 @@ pub struct FileCli {
 pub struct ApiCli {}
 
 impl Run for Cli {
-    fn run(&self, _db: &Database) {
-        unimplemented!();
+    fn run(&self, db: &Database) {
+        match self {
+            Cli::File(fc) => {
+                let systems = edsm::json(&fc.path);
+                let bar = ProgressBar::new(systems.len() as u64);
+                bar.set_style(ProgressStyle::default_bar()
+                    .template("[{elapsed_precise}/{eta_precise}] {bar:40} {pos:>7}/{len:7} ({percent}%) {msg}")
+                    .unwrap()
+                    .progress_chars("##-"));
+                for system in bar.wrap_iter(systems.into_iter()) {
+                    if system.id.is_none() || system.coords.is_none() {
+                        continue;
+                    }
+                    let result = task::block_on(async {
+                        System::create(db,
+                            system.id.unwrap() as i64,
+                            &system.name,
+                            system.coords.unwrap(),
+                            None, None, None, None, None, None,
+                            Utc::now(),
+                        ).await
+                    });
+                    match result {
+                        Ok(_) => bar.set_message(format!("[EDSM] {}", system.name)),
+                        Err(err) => bar.set_message(format!("[EDSM ERROR] {}", err)),
+                    }
+                }
+            },
+            Cli::Api(_ac) => {
+                unimplemented!();
+            },
+        }
     }
 }
+
