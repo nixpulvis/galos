2088ea3df255a448a394d03eb1ef2b65afdaa2be
Nathan Lilienthal <nathan@nixpulvis.com>
2024-08-09 13:27:38 -0400

    Importing from the EDSM API!

--- src/bin/galos-sync/edsm.rs
+++ src/bin/galos-sync/edsm.rs
@@ -1,4 +1,5 @@
 #![cfg(unix)]
+use std::collections::HashMap;
 use structopt::StructOpt;
 use indicatif::{ProgressBar, ProgressStyle};
 use async_std::task;
@@ -16,46 +17,85 @@ pub enum Cli {
 #[derive(StructOpt, Debug)]
 pub struct FileCli {
     // TODO: Type as a path.
-    // TODO: Default, when not provided?
     #[structopt(name = "PATH")]
     pub path: String,
 }
 
-// TODO: -s single system, -S systems query?
 #[derive(StructOpt, Debug)]
-pub struct ApiCli {}
+pub struct ApiCli {
+    #[structopt(name = "NAME")]
+    pub name: String,
+
+    #[structopt(name = "cube", long, short)]
+    pub cube: Option<u32>,
+    #[structopt(name = "sphere", long, short)]
+    pub sphere: Option<u32>,
+}
+
+impl Cli {
+    fn create_vec(db:&Database, systems: Vec<edsm::system::System>) {
+        let mut imported = 0;
+        let mut errors = HashMap::new();
+        let bar = ProgressBar::new(systems.len() as u64);
+        bar.set_style(ProgressStyle::default_bar()
+            .template("[{elapsed_precise}/{eta_precise}] {bar:40} {pos:>7}/{len:7} ({percent}%) {msg}")
+            .unwrap()
+            .progress_chars("##-"));
+        for system in bar.wrap_iter(systems.into_iter()) {
+            if system.id.is_none() || system.coords.is_none() {
+                continue;
+            }
+            let result = task::block_on(async {
+                let r = System::create(db,
+                    system.id.unwrap() as i64,
+                    &system.name,
+                    system.coords.unwrap(),
+                    system.information.population,
+                    system.information.security,
+                    system.information.government,
+                    system.information.allegiance,
+                    system.information.economy,
+                    system.information.second_economy,
+                    Utc::now(),
+                ).await;
+                r
+            });
+            match result {
+                Ok(_) => {
+                    bar.set_message(format!("[EDSM] {}", system.name));
+                    imported += 1;
+                },
+                Err(err) => {
+                    bar.set_message(format!("[EDSM ERROR] {}", err));
+                    errors.entry(err.to_string())
+                        .and_modify(|ns: &mut Vec<String>| ns.push(system.name.clone()))
+                        .or_insert(vec![system.name]);
+                },
+            }
+        }
+        println!("Imported {} systems.", imported);
+        for (err, system_names) in errors {
+            println!("Failed to import {} systems:\n{}: {}", system_names.len(), err, system_names.join(", "));
+        }
+    }
+}
 
 impl Run for Cli {
     fn run(&self, db: &Database) {
         match self {
             Cli::File(fc) => {
                 let systems = edsm::json(&fc.path);
-                let bar = ProgressBar::new(systems.len() as u64);
-                bar.set_style(ProgressStyle::default_bar()
-                    .template("[{elapsed_precise}/{eta_precise}] {bar:40} {pos:>7}/{len:7} ({percent}%) {msg}")
-                    .unwrap()
-                    .progress_chars("##-"));
-                for system in bar.wrap_iter(systems.into_iter()) {
-                    if system.id.is_none() || system.coords.is_none() {
-                        continue;
-                    }
-                    let result = task::block_on(async {
-                        System::create(db,
-                            system.id.unwrap() as i64,
-                            &system.name,
-                            system.coords.unwrap(),
-                            None, None, None, None, None, None,
-                            Utc::now(),
-                        ).await
-                    });
-                    match result {
-                        Ok(_) => bar.set_message(format!("[EDSM] {}", system.name)),
-                        Err(err) => bar.set_message(format!("[EDSM ERROR] {}", err)),
-                    }
-                }
+                Cli::create_vec(db, systems);
             },
-            Cli::Api(_ac) => {
-                unimplemented!();
+            Cli::Api(ac) => {
+                let systems = if let Some(n) = ac.sphere {
+                    edsm::api::systems_sphere(&ac.name, Some(n as f64), None).unwrap()
+                } else if let Some(n) = ac.cube {
+                    edsm::api::systems_cube(&ac.name, Some(n as f64)).unwrap()
+                } else {
+                    edsm::api::systems(&ac.name).unwrap()
+                };
+                Cli::create_vec(db, systems);
             },
         }
     }
