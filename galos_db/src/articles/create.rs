0ddeb399ab4ced0b221cb85886de866d0dbedfcf
Nathan Lilienthal <nathan@nixpulvis.com>
2024-08-14 12:07:13 -0400

    Start refactoring the db code into smaller files

---
+++ galos_db/src/articles/create.rs
@@ -0,0 +1,32 @@
+use crate::{Database, Error};
+use sqlx::types::chrono::NaiveDate;
+use super::Article;
+
+impl Article {
+    pub async fn create(
+        db: &Database,
+        title: Option<String>,
+        date: NaiveDate,
+        body: String,
+    ) -> Result<Self, Error> {
+        let row = sqlx::query!(
+            "
+            INSERT INTO articles (title, date, body)
+            VALUES ($1, $2, $3)
+            RETURNING *
+            ",
+            title,
+            date,
+            body
+        )
+        .fetch_one(&db.pool)
+        .await?;
+
+        Ok(Article {
+            id: row.id,
+            title: row.title,
+            date: row.date,
+            body: row.body,
+        })
+    }
+}
--- galos_db/src/articles/fetch.rs
+++ galos_db/src/articles/fetch.rs
@@ -1,42 +1,8 @@
 use crate::{Database, Error, Page};
 use sqlx::types::chrono::NaiveDate;
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct Article {
-    pub id: i32,
-    pub title: Option<String>,
-    pub date: NaiveDate,
-    pub body: String,
-}
+use super::Article;
 
 impl Article {
-    pub async fn create(
-        db: &Database,
-        title: Option<String>,
-        date: NaiveDate,
-        body: String,
-    ) -> Result<Self, Error> {
-        let row = sqlx::query!(
-            "
-            INSERT INTO articles (title, date, body)
-            VALUES ($1, $2, $3)
-            RETURNING *
-            ",
-            title,
-            date,
-            body
-        )
-        .fetch_one(&db.pool)
-        .await?;
-
-        Ok(Article {
-            id: row.id,
-            title: row.title,
-            date: row.date,
-            body: row.body,
-        })
-    }
-
     pub async fn fetch(db: &Database, id: i32) -> Result<Self, Error> {
         let row = sqlx::query!(
             "
