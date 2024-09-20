0e3dfeab621e9bb6810be3de91a4764435e029d1
Nathan Lilienthal <nathan@nixpulvis.com>
2024-09-20 13:42:32 -0400

    Removing migration that wasn't ready yet.

--- galos_db/migrations/20240914164236_create_systems_notify.down.sql
+++
@@ -1,4 +0,0 @@
-DROP TRIGGER systems_notify_update ON systems;
-DROP TRIGGER systems_notify_insert ON systems;
-DROP TRIGGER systems_notify_delete ON systems;
-DROP FUNCTION systems_update_notify();
