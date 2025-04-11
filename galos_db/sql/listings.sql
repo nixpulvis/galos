48ba6f0b4b93c13bc48c346486ecfd551d311674
Nathan Lilienthal <nathan@nixpulvis.com>
2025-04-10 20:21:19 -0400

    Update listings SQL example to return useful data

--- galos_db/sql/listings.sql
+++ galos_db/sql/listings.sql
@@ -1,17 +1,11 @@
 SELECT
-   listings.name,
-   sell_price,
-   demand,
    systems.name,
    stations.name,
-   stations.landing_pads,
-   stations.dist_from_star_ls,
-   (SELECT ST_3DDistance(origin.position, systems.position)
-      FROM systems origin
-      WHERE name = 'MELIAE') AS distance
+   listings.*
 FROM listings
 JOIN markets ON listings.market_id = markets.id
-JOIN systems ON systems.address = markets.system_address
-JOIN stations ON markets.station_name = stations.name
-WHERE listings.name ILIKE 'lowtemperaturediamond'
-ORDER BY sell_price DESC;
+JOIN stations ON (stations.name = markets.station_name AND
+                  stations.system_address = markets.system_address)
+JOIN systems ON systems.address = stations.system_address
+WHERE listings.name = 'tritium'
+ORDER BY listings.listed_at DESC;
